d8c3311b2cf88ac2bd3491919afe930782584743
EvgeniyKuzin <vaglover@yandex.ru>
2023-03-21 18:38:37 +0300

    model modification

--- JSON parsing/Models/Valute.swift
+++ JSON parsing/Models/Valute.swift
@@ -9,17 +9,7 @@ import Foundation
 
 struct Valute: Decodable {
     let Date: String
-    let Valute: ValuteName
-}
-
-struct ValuteName: Decodable {
-    let GBP: ValuteInfo
-    let BYN: ValuteInfo
-    let USD: ValuteInfo
-    let EUR: ValuteInfo
-    let KZT: ValuteInfo
-    let TRY: ValuteInfo
-    let UAH: ValuteInfo
+    let Valute: [String: ValuteInfo]
 }
 
 struct ValuteInfo: Decodable {
